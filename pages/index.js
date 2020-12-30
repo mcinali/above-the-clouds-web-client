e45f0e7da96121bb9ad8dac2cde4768bca9160aa
Mark Cinali <mcinali@Marks-MBP.fios-router.home>
2020-12-30 16:00:48 -0500

    set up landing page

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,20 @@
-import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
-export default function Home() {
+export default function Landing() {
   return (
     <div className={styles.container}>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a> Above the Clouds!</a>
+        <h1 className={styles.title}>Welcome to
+          <a> Above the Clouds!</a>
         </h1>
-
-        <p className={styles.description}>
-          A Space for Meaningful Conversations
+        <p className={styles.description}>A Space for Meaningful Conversations</p>
+        <button className={styles.registrationButton}>
+          <Link href="/register">Register</Link>
+        </button>
+        <p className={styles.textlink}>
+          <Link href="/login">Already have an account? Login here</Link>
         </p>
       </main>
 
