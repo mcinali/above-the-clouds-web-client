a04dd76cfd34bb40a5b3dac7264cc5310476eba3
Mark Cinali <mcinali@Marks-MBP.fios-router.home>
2020-12-31 13:20:51 -0500

    registration form skeleton

--- pages/register.js
+++ pages/register.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import styles from '../styles/Common.module.css'
 import Link from 'next/link'
 
@@ -9,9 +10,7 @@ export default function Register() {
           <a> Above the Clouds</a>
         </h1>
         <p className={styles.description}>A Space for Meaningful Conversations</p>
-        <button className={styles.registrationButton}>
-          <Link href="/discovery">Register</Link>
-        </button>
+        <RegistrationForm></RegistrationForm>
       </main>
 
       <footer className={styles.footer}>
@@ -25,4 +24,47 @@ export default function Register() {
       </footer>
     </div>
   )
+}
+
+class RegistrationForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: '',
+      password: '',
+      firstname: '',
+      lastname: '',
+      email: '',
+      phone: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({[event.target.name]: event.target.value.trim()})
+  }
+
+  handleSubmit(event){
+    alert(this.state)
+    // TO DO: POST account request
+    event.preventDefault()
+  }
+
+  render(){
+    return (
+      <form onSubmit={this.handleSubmit} className={styles.form}>
+        <input name="username" placeholder="username" value={this.state.username} onChange={this.handleChange} className={styles.inputWide}/>
+        <input name="password" placeholder="password" value={this.state.password} onChange={this.handleChange} className={styles.inputWide}/>
+        <input name="firstname" placeholder="firstname" value={this.state.firstname} onChange={this.handleChange} className={styles.inputNarrowLeft}/>
+        <input name="lastname" placeholder="lastname" value={this.state.lastname} onChange={this.handleChange} className={styles.inputNarrowRight}/>
+        <input name="email" placeholder="email" value={this.state.email} onChange={this.handleChange} className={styles.inputWide}/>
+        <input name="phone" placeholder="phone number" value={this.state.phone} onChange={this.handleChange} className={styles.inputWide}/>
+        <p></p>
+        <button type="submit" className={styles.registrationButton}>
+          <Link href="/register">Register</Link>
+        </button>
+    </form>
+    )
+  }
 }
