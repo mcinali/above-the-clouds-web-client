abb3fdf878af54f9e93dd4545b26b13b604972b4
Mark Cinali <mcinali@Marks-MBP.fios-router.home>
2021-01-10 15:24:03 -0500

    additional discovery styling + beginning of connections section

--- pages/discovery.js
+++ pages/discovery.js
@@ -3,16 +3,34 @@ import Link from 'next/link'
 import Router from "next/router"
 import commonStyles from '../styles/Common.module.css'
 import discoveryStyles from '../styles/Discovery.module.css'
+import connectionsStyles from '../styles/Connections.module.css'
 const { hostname } = require('../config')
 const axios = require('axios')
-import Cookies from 'universal-cookie';
+import Cookies from 'universal-cookie'
+import Image from 'next/image'
 
 export default function Discover() {
   return (
     <div className={commonStyles.container}>
-      <header className={commonStyles.header}></header>
-      <div id="leftPanel" className={discoveryStyles.leftPanel}>Hello</div>
-      <div id="rightPanel" className={discoveryStyles.rightPanel}>{DiscoveryStreams()}</div>
+      <div className={commonStyles.navbar}>
+        <div className={commonStyles.navBarContent}>
+          <div className={commonStyles.navbarItemCenter}>
+            <a className={commonStyles.navbarLink}>Feedback</a>
+          </div>
+          <div className={commonStyles.navbarItemRight}>
+            <Image src='/bitmoji.png' width='30' height='30' className={commonStyles.image} />
+          </div>
+        </div>
+      </div>
+      <div className={discoveryStyles.panelLeft}>
+        {Connections()}
+      </div>
+      <div className={discoveryStyles.panelRight}>
+        <div className={discoveryStyles.newStreamContainer}>
+          <button className={discoveryStyles.newStreamButton}>New Stream</button>
+        </div>
+        {DiscoveryStreams()}
+      </div>
     </div>
   )
 }
@@ -21,6 +39,48 @@ export default function Discover() {
 // Discovery component (styling + join button + start new button)
 // Connections component
 // Header/Account component
+
+function Connections(){
+  const [connections, setConnections] = useState([])
+  const [outboundRequests, setOutboundRequests] = useState([])
+  const [inboundRequests, setInboundRequests] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [error, setError] = useState({})
+
+  useEffect(() => {
+    setIsLoading(true)
+    const cookie = new Cookies()
+    const accountId = cookie.get('accountId')
+    axios.get(hostname+`/connections/${accountId}`)
+         .then(res => {
+           if(isLoading){
+             setConnections(res.data.connections)
+             setInboundRequests(res.data.inboundRequests)
+             setOutboundRequests(res.data.outboundRequests)
+             setIsLoading(false)
+           }
+         })
+         .catch(error => {
+           if (error.response && error.response.data){
+             if (isLoading){
+               setError(error.response.data)
+               setIsLoading(false)
+             }
+           }
+         })
+    return
+  }, [])
+
+  return (
+    <div className={connectionsStyles.connectionsContainer}>
+      <div className={connectionsStyles.tab}>
+        <button className={connectionsStyles.button}>Connections</button>
+        <button className={connectionsStyles.button}>Requests</button>
+        <button className={connectionsStyles.button}>Outbound</button>
+      </div>
+    </div>
+  )
+}
 
 function DiscoveryStreams() {
   const [streams, setStreams] = useState([])
@@ -35,7 +95,6 @@ function DiscoveryStreams() {
          .then(res => {
            if(isLoading){
              setStreams(res.data)
-             setIndex(0)
              setIsLoading(false)
            }
          })
@@ -48,20 +107,23 @@ function DiscoveryStreams() {
            }
          })
     return
-  }, [isLoading])
+  }, [])
 
   return (
-    <div className={discoveryStyles.cardList}>
-      <div>
-        {console.log(streams)}
+
+    <div id="cardList" className={discoveryStyles.cardList}>
+      <div id="cardContainer">
         {streams.map((stream, index) =>
-          <div key={index.toString()} className={discoveryStyles.card}>
-            <p>Topic: {stream.info.topic}</p>
+          <div id="card" key={index.toString()} className={discoveryStyles.card}>
+            <p className={discoveryStyles.speakerAccessibility}>{stream.info.speakerAccessibility}</p>
+            <h1 className={discoveryStyles.topic}>{stream.info.topic}</h1>
             <p>Capacity: {stream.info.capacity}</p>
             <p>Participants:</p>
             <ul>
               {stream.participants.map((participant,index) => <li key={index.toString()}>{`${participant.username} (${participant.firstname} ${participant.lastnameInitial})`}</li>)}
             </ul>
+            <button className={discoveryStyles.cardButton}>Join</button>
+            <button className={discoveryStyles.cardButton}>Fork</button>
           </div>
         )}
       </div>
