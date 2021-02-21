2a0c3013ca14f97d2fe56770579dffa3f114f229
Mark Cinali <mcinali@Marks-MBP.fios-router.home>
2021-02-21 14:54:11 -0500

    gke deployment files/scripts

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const env = process.env.NODE_ENV
+const env = process.env.RUNTIME_ENV
 const localhost = 'http://localhost:8080'
 const gcphost = 'https://api.abovethecloudsapp.com'
 const hostname = (env=='prod') ? gcphost : localhost
