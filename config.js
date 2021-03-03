9619dfb99dffb34a1111159d3572b56aa08ad29b
Mark Cinali <mcinali@Marks-MBP.fios-router.home>
2021-03-03 13:08:35 -0500

    refactored backend host configuration

--- config.js
+++ config.js
@@ -2,17 +2,16 @@
 const env = process.env.RUNTIME_ENV
 // Create local URLs for APIs/sockets
 const localhostURL = 'http://localhost'
-const restPort = '8080'
+const port = '8080'
 const socketPort = '8000'
-const localhost = localhostURL + ':' + restPort
-const sockethost = localhostURL + ':' + socketPort
+const localhost = localhostURL + ':' + port
+const localsockethost = localhostURL + ':' + socketPort
 // Create GCP URLs for APIs/sockets
 const gcphost = 'https://api.abovethecloudsapp.com'
-const gcpsockethost = 'https://api.abovethecloudsapp.com:8000'
 // Set hostname based on runtime environment
 const hostname = (env=='prod') ? gcphost : localhost
 // Set socket hostname based on runtime environment
-const sockethostname = (env=='prod') ? gcpsockethost : sockethost
+const sockethostname = (env=='prod') ? gcphost : localsockethost
 
 module.exports = {
   hostname,
