614a6fdadbf7c07992446024346b848da5100cfb
Mark Cinali <mcinali@Marks-MBP.fios-router.home>
2021-03-16 23:11:33 -0400

    (WIP) menu guide options

--- components/menuGuide.js
+++ components/menuGuide.js
@@ -30,22 +30,42 @@ export default function MenuGuide(showMenu, setShowMenu, setShowNewStreamModal) 
     }
   }
 
+  function broadcast(){
+    try {
+      // TO DO: broadcast
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return (
     <div>
       <div className={menuGuideStyles.background} style={displayMenuGuide}></div>
       <div className={menuGuideStyles.modal} style={displayMenuGuide}>
-        <div className={menuGuideStyles.title}>Welcome to
-          <a> Above the Clouds!</a>
+        <div className={menuGuideStyles.title}>
+          Looks like there's <a>no activity</a> at the moment.
         </div>
         <div className={menuGuideStyles.cardContainer}>
           <button className={menuGuideStyles.card} onClick={function(){newStream()}}>
             <div className={menuGuideStyles.cardTitle}> <a>Start</a> a stream</div>
-            <div className={menuGuideStyles.cardDescription}>Start a new stream, invite friends, and hang out until your followers and/or invitees to join you.</div>
+            <div className={menuGuideStyles.cardDescription}>Start a conversation. Invite others.</div>
+          </button>
+          <button className={menuGuideStyles.card} onClick={function(){broadcast()}}>
+            <div className={menuGuideStyles.cardTitle}><a>Broadcast</a> and hang out</div>
+            <div className={menuGuideStyles.cardDescription}>
+              Let people know you'll be hanging out for a while.
+            </div>
           </button>
           <button className={menuGuideStyles.card} onClick={function(){discovery()}}>
-            <div className={menuGuideStyles.cardTitle}><a>Discover</a> streams</div>
+            <div className={menuGuideStyles.cardTitle}><a>Hang out</a> quietly</div>
             <div className={menuGuideStyles.cardDescription}>
-              Discover your followers streams and streams you've been invited to.
+              Get notified when things are happening.
+            </div>
+          </button>
+          <button className={menuGuideStyles.card} onClick={function(){discovery()}}>
+            <div className={menuGuideStyles.cardTitle}><a>Come back</a> later</div>
+            <div className={menuGuideStyles.cardDescription}>
+              We'll help you find a good time to return.
             </div>
           </button>
         </div>
