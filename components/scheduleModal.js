7b187541cbce7307dd12bc6855d6c6612753a828
Mark Cinali <mcinali@Marks-MBP.fios-router.home>
2021-03-18 17:11:34 -0400

    schedule modal with fake data

--- components/scheduleModal.js
+++ components/scheduleModal.js
@@ -3,8 +3,9 @@ import Router from 'next/router'
 import { createPictureURLFromArrayBufferString } from '../utilities'
 import modalStyles from '../styles/Modal.module.css'
 import discoveryStyles from '../styles/Discovery.module.css'
-import userStyles from '../styles/Users.module.css'
+import scheduleStyles from '../styles/Schedule.module.css'
 const axios = require('axios')
+const { futureStreams } = require('../data/futureStreams')
 
 const Image = React.memo(function Image({ src }) {
   return <img className={userStyles.image} src={createPictureURLFromArrayBufferString(src)}/>
@@ -12,6 +13,7 @@ const Image = React.memo(function Image({ src }) {
 
 export default function ScheduleModal(hostname, accountId, accessToken, showModal, setShowModal, setNewStreamShowModal){
   const [displayModal, setDisplayModal] = useState({'display':'none'})
+  const [scheduledStreams, setScheduledStreams] = useState([])
 
   useEffect(() => {
     if (showModal){
@@ -20,6 +22,16 @@ export default function ScheduleModal(hostname, accountId, accessToken, showModa
       setDisplayModal({'display':'none'})
     }
   }, [showModal])
+
+  useEffect(() => {
+    setScheduledStreams(futureStreams)
+  }, [futureStreams])
+
+  useEffect(() => {
+    scheduledStreams.map((scheduledStream, index) => {
+      console.log(scheduledStream)
+    })
+  }, [scheduledStreams])
 
   function scheduleStream(){
     try {
@@ -44,6 +56,30 @@ export default function ScheduleModal(hostname, accountId, accessToken, showModa
       <div className={modalStyles.modal} style={displayModal}>
         <div className={discoveryStyles.newStreamContainer}>
           <button className={discoveryStyles.newStreamButton} onClick={function(){scheduleStream()}}>Schedule Stream+</button>
+        </div>
+        <div className={scheduleStyles.container}>
+          <div className={scheduleStyles.day}>Friday, March 19th</div>
+          {scheduledStreams.map((scheduledStream, index) => {
+            return (
+              <div key={index.toString()} className={scheduleStyles.scheduleContainer}>
+                <div className={scheduleStyles.scheduledStreamTitle}>
+                  {scheduledStream.parsedTOD + ' - ' + scheduledStream.topic}
+                </div>
+                <div className={scheduleStyles.scheduledStreamOrganizer}>
+                  {`Organizer:  ${scheduledStream.creator.firstname} ${scheduledStream.creator.lastname}`}
+                </div>
+                <div className={scheduleStyles.scheduledStreamInterest}>
+                  {'Interest: '}
+                  {scheduledStream.interest.map((account, index2) => {
+                    return (
+                      <div key={index2.toString()}>{`${account.firstname} ${account.lastname}`}</div>
+                    )
+                  })}
+                </div>
+                <button className={scheduleStyles.notificationButton}>Notify Me</button>
+              </div>
+            )
+          })}
         </div>
         <div className={modalStyles.closeButtonContainer}>
           <button className={modalStyles.closeButton} onClick={function(){closeModal()}}>close</button>
